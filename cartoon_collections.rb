725052ad980291d76cee677d094a809002b7fac9
Brian Salemi <briandsalemi@gmail.com>
2019-04-25 17:06:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,9 +5,9 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.collect do |planeteer| 
+  array.collect do |planeteer|
     planeteer.capitalize << "!"
-  end 
+  end
 end
 
 def long_planeteer_calls# code an argument here
