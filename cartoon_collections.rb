d2ce16491c4890b9ce37add70d026cb8059fc509
Brian Salemi <briandsalemi@gmail.com>
2019-04-25 17:32:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   if array.include?(cheese_types) == true
       array.find do |food|
-        food.cheese_types
+        puts food.cheese_types
         end
   else
     return nil
